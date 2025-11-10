f9096e07d592ec5bfd42d551816a77998a5d01fe
dikiptr02 <oktavian.dwikiputra@gmail.com>
2025-11-10 13:46:06 +0700

    data validation added

--- src/MlProject/components/data_ingestion.py
+++ src/MlProject/components/data_ingestion.py
@@ -3,7 +3,7 @@ import urllib.request as request
 import zipfile
 from mlProject import logger
 from mlProject.utils.common import get_size
-from src.mlProject.entity.config_entity import (DataIngestionConfig)
+from src.mlProject.entity.config_entity import DataIngestionConfig
 from pathlib import Path
 
 
